15ce003da9d8fc2dae98060da4801697363e1d3d
Arjun <arjunp0710@gmail.com>
2021-08-22 20:00:14 +0530

    Added Edit `Stores`

--- views/stores.py
+++ views/stores.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, redirect
 import json
 from models.store import Store
 
@@ -20,3 +20,21 @@ def create_store():
         query = json.loads(request.form['query'])
         Store(name, url_prefix, tag_name, query).save_to_db()
     return render_template('stores/new_store.html')
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit_store(store_id):
+    store = Store.get_by_id(store_id)
+    if request.method == 'POST':
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+        store.name = name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+        store.save_to_db()
+        return redirect(url_for('.index'))
+
+    return render_template('stores/edit_store.html', store=store)
